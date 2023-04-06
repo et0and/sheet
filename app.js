ec9000fd5ffbfce5ce380184c84ea5f65b2ddf1e
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-04-06 21:50:59 +1200

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function applyExifOrientation(image) {
 
 async function generateContactSheet(images) {
   const columns = 4;
-  const rows = 10;
+  const rows = 8;
   const margin = 20;
   const padding = 20;
   const cellWidth = 180;
